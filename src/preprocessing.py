5e35a658eeef999cf290264bc2d24ad29df4fc85
Matias Rolon <matirolon96@gmail.com>
2021-09-19 20:37:33 -0300

    Create preprocessing class

---
+++ src/preprocessing.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 10 21:58:49 2021
+
+@author: matiasrolon
+"""
+
+# Libraries
+import pandas as pd
+import numpy as np
+
+
+class Preprocessor:
+
+    def __init__(self, path_folder="./"):
+        self.path_folder = path_folder              # Carpeta base donde se encuentran los datos, graban archivos nuevos, etc.
+
+    def write(self, data, path_write='./'):
+        if isinstance(data, pd.DataFrame):          # Si es un dataframe.
+            data.to_csv(path_write)                 # Grabo archivo en path.
+
+    def proproccess(self, path_data,  path_emotions="./emotions.txt", tag="undefined"):
+        print("Preprocesamiento de dataset en ", path_data)
+
+        # Carga data
+        data = pd.read_csv(
+            path_data,
+            sep='\t',
+            names=["comment", "emotions", "code"]
+        )
+        data = data.drop(columns="code")
+        data["tag"] = tag
+
+        # Carga emociones
+        emotions = []
+        with open(path_emotions) as file_emotions:
+            lines_emotions = file_emotions.readlines()
+            for line in lines_emotions:                         # Setea el nombre de todas las emociones posibles en un array
+                column_name = line.rstrip()
+                emotions.append(column_name)
+                data[column_name] = 0
+
+        for index, row in data.iterrows():
+            row_emotion_ids = row["emotions"].split(',')        # Extrae las emociones de ese comentario
+            for emotion_id in row_emotion_ids:
+                column_name = emotions[int(emotion_id)]
+                data.loc[index, column_name] = 1                # Setea con 1 aquellas emociones que estan en el comentario.
+
+        return data
--- src/test-bert.py
+++ src/test-bert.py
@@ -6,9 +6,10 @@ Created on Tue Aug 10 21:58:49 2021
 @author: matiasrolon
 """
 
-# Libraries
+# Libraries y clases
 import pandas as pd
 import numpy as np
+from preprocessing import Preprocessor
 
 
 # Constants
@@ -16,40 +17,13 @@ PATH_FOLDER_DATA = "../dataset/multilabel-emotions-clasifications"
 PATH_TEST = PATH_FOLDER_DATA+ "/test.tsv"
 PATH_TRAIN = PATH_FOLDER_DATA + "/train.tsv"
 PATH_EMOTIONS = PATH_FOLDER_DATA + "/emotions.txt"
-
-
-def proproccess_dataset(path_file, tag="undefined"):
-    # Carga data
-    data = pd.read_csv(
-        path_file,
-        sep='\t',
-        names=["comment", "emotions", "code"]
-    )
-    data = data.drop(columns="code")
-    data["tag"] = tag
-
-    # Carga emociones
-    emotions = []
-    with open(PATH_EMOTIONS) as file_emotions:
-        lines_emotions = file_emotions.readlines()
-        for line in lines_emotions:                         # Setea el nombre de todas las emociones posibles en un array
-            column_name = line.rstrip()
-            emotions.append(column_name)
-            data[column_name] = 0
-
-    for index, row in data.iterrows():
-        row_emotion_ids = row["emotions"].split(',')        # Extrae las emociones de ese comentario
-        for emotion_id in row_emotion_ids:
-            column_name = emotions[int(emotion_id)]
-            data.loc[index, column_name] = 1                # Setea con 1 aquellas emociones que estan en el comentario.
-
-    return data
-
+NAME_PREPROCESSING_DATA = 'preprocess_emotions.csv'
 
 if __name__ == "__main__":
-    data_train = proproccess_dataset(PATH_TRAIN, "train")
-    data_test = proproccess_dataset(PATH_TEST, "test")
+    pp = Preprocessor(PATH_FOLDER_DATA)
+    data_train = pp.proproccess(PATH_TRAIN, PATH_EMOTIONS, "train")
+    data_test = pp.proproccess(PATH_TEST, PATH_EMOTIONS, "test")
 
     data = pd.concat([data_train, data_test])
     print(data)
-    data.to_csv('preprocess_emotions.csv')
+    pp.write(data, NAME_PREPROCESSING_DATA)
